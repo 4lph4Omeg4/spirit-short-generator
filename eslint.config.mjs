92824b8a25d0fa4f9354a21455708107503c040f
SH4M4NI4K <135929616+4lph4Omeg4@users.noreply.github.com>
2025-12-05 21:17:55 +0100

    analytics added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig, globalIgnores } from "eslint/config";
+import { Analytics } from "@vercel/analytics/next";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+
 
 const eslintConfig = defineConfig([
   ...nextVitals,
